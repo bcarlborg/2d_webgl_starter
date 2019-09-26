89f5e35cc4532c21db102f3a10ef7ab559bd9e30
bcarlborg <bcarlborg@gmail.com>
2019-09-26 15:11:45 +0200

    simple server implementation

---
+++ simpleServer.js
@@ -0,0 +1,6 @@
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 4000;
+console.log('your site is running at port: ', port);
+app.use(express.static(__dirname + '/src'));
+app.listen(port);
