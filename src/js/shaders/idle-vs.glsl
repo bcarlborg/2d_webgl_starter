6451627e2da7ff18d5b666be36a9bb5f59d48416
bcarlborg <bcarlborg@gmail.com>
2019-09-29 14:18:09 +0200

    removing original fragment shaders

--- src/js/shaders/idle-vs.glsl
+++
@@ -1,8 +0,0 @@
-import Sahder from '../Shader.js';
-Shader.source[document.currentScript.src.split('js/shaders/')[1]] = `#version 300 es
-  in vec4 vertexPosition;
-
-  void main(void) {
-    gl_Position = vertexPosition;
-  }
-`;
