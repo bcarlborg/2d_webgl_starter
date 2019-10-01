2b3b88325a68393d41fb13833bd3bb7ea7dd6db3
bcarlborg <bcarlborg@gmail.com>
2019-10-01 09:16:00 +0200

    moving circle rendering into a scene object

--- src/js/Scene.js
+++ src/js/Scene.js
@@ -1,23 +1,12 @@
-import Shader from './Shader.js';
-import Program from './Program.js';
-import TriangleGeometry from './geometries/TriangleGeometry.js';
-import CircleGeometry from './geometries/CircleGeometry.js';
-import IdleVertexShader from './shaders/IdleVertexShader.js';
-import SolidFragmentShader from './shaders/SolidFragmentShader.js';
+import UniverseScene from './scenes/UniverseScene.js';
 import MyColors from './helpers/MyColors.js';
 /* exported Scene */
 // eslint-disable-next-line
 export default class Scene {
   constructor(gl) {
-    this.vsIdle = new Shader(gl, gl.VERTEX_SHADER, IdleVertexShader);
-    this.fsSolid = new Shader(gl, gl.FRAGMENT_SHADER, SolidFragmentShader);
-    this.solidProgram = new Program(gl, this.vsIdle, this.fsSolid);
-
-    // this.triangleGeometry = new TriangleGeometry(gl);
-    this.circleGeometry = new CircleGeometry(gl);
+    this.UniverseScene = new UniverseScene(gl);
 
     this.background = MyColors.getRandomColor('900');
-    this.forGround = MyColors.getRandomColor('100');
   }
 
   resize(gl, canvas) {
@@ -39,13 +28,8 @@ export default class Scene {
   // eslint-disable-next-line
   update(gl, keysPressed) {
     this.drawBackground(gl);
-    gl.useProgram(this.solidProgram.glProgram);
-
-    const colorArray = [this.forGround.r, this.forGround.g, this.forGround.b, 1.0];
-    const colorUniformLoc = gl.getUniformLocation(this.solidProgram.glProgram, 'u_color');
-    gl.uniform4fv(colorUniformLoc, colorArray);
 
     // this.triangleGeometry.draw();
-    this.circleGeometry.draw();
+    this.UniverseScene.draw();
   }
 }
---
+++ src/js/scenes/UniverseScene.js
@@ -0,0 +1,32 @@
+import Shader from '../Shader.js';
+import Program from '../Program.js';
+import IdleVertexShader from '../shaders/IdleVertexShader.js';
+import SolidFragmentShader from '../shaders/SolidFragmentShader.js';
+import CircleGeometry from '../geometries/CircleGeometry.js';
+import MyColors from '../helpers/MyColors.js';
+
+export default class UniverseScene {
+  constructor(gl) {
+    this.gl = gl;
+    this.centerCircle = new CircleGeometry(gl);
+
+    this.vsIdle = new Shader(gl, gl.VERTEX_SHADER, IdleVertexShader);
+    this.fsSolid = new Shader(gl, gl.FRAGMENT_SHADER, SolidFragmentShader);
+    this.solidProgram = new Program(gl, this.vsIdle, this.fsSolid);
+
+    this.forGround = MyColors.getRandomColor('100');
+  }
+
+  draw() {
+    const { gl } = this;
+
+    gl.useProgram(this.solidProgram.glProgram);
+
+    const colorArray = [this.forGround.r, this.forGround.g, this.forGround.b, 1.0];
+    const colorUniformLoc = gl.getUniformLocation(this.solidProgram.glProgram, 'u_color');
+    this.gl.uniform4fv(colorUniformLoc, colorArray);
+
+    // this.triangleGeometry.draw();
+    this.centerCircle.draw();
+  }
+}
