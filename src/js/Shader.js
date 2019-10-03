711c1be7d77620656c8892409d887a7e47a43c6c
bcarlborg <bcarlborg@gmail.com>
2019-10-03 14:47:11 +0200

    prettier

--- src/js/Shader.js
+++ src/js/Shader.js
@@ -1,4 +1,5 @@
-"use strict";
+'use strict';
+
 /* exported Shader */
 class Shader {
   constructor(gl, shaderType, sourceFileName) {
@@ -7,19 +8,19 @@ class Shader {
     if (source) {
       const marked = source.replace(
         /[^\x00-\x7F]/g,
-        match => "\x1b[46m" + match + "\x1b[30m"
+        (match) => `\x1b[46m${match}\x1b[30m`,
       );
       if (marked !== source) {
-        console.error("\x1b[46mNON-ASCII:\x1b[30m\n" + marked);
+        console.error(`\x1b[46mNON-ASCII:\x1b[30m\n${marked}`);
         throw new Error(
-          `Shader source file ${sourceFileName} has non-ASCII characters.`
+          `Shader source file ${sourceFileName} has non-ASCII characters.`,
         );
       }
       this.glShader = gl.createShader(shaderType);
       gl.shaderSource(this.glShader, source);
     } else {
       throw new Error(
-        `Shader ${sourceFileName} not found. Check spelling, and whether the essl file is embedded into the html file.`
+        `Shader ${sourceFileName} not found. Check spelling, and whether the essl file is embedded into the html file.`,
       );
     }
     gl.compileShader(this.glShader);
@@ -27,14 +28,13 @@ class Shader {
       console.error(
         `Error in shader ${sourceFileName}:
         ${gl
-          .getShaderInfoLog(this.glShader)
-          .replace(/ERROR: 0/g, Shader.sourcePathURL + sourceFileName)}`
+    .getShaderInfoLog(this.glShader)
+    .replace(/ERROR: 0/g, Shader.sourcePathURL + sourceFileName)}`,
       );
       throw new Error(`Shader ${sourceFileName} had compilation errors.`);
     }
   }
 }
 
-Shader.sourcePathURL =
-  document.currentScript.src.split("Shader.js")[0] + "shaders/";
+Shader.sourcePathURL = `${document.currentScript.src.split('Shader.js')[0]}shaders/`;
 Shader.source = {};
