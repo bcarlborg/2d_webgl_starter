35c07807f86f666a78c763d626a8187b78aa1d5b
bcarlborg <bcarlborg@gmail.com>
2019-10-08 16:24:59 +0200

    puling added

--- src/js/gameObjects/OrbitingObject.js
+++ src/js/gameObjects/OrbitingObject.js
@@ -6,8 +6,7 @@ import GameObject from '../GameObject.js';
 export default class OrbitingObject extends GameObject {
   constructor(mesh, timeObject) {
     super(mesh, timeObject);
-
-    this.rotationRadius = 2.0;
+    this.rotationRadius = 1.0;
   }
 
   orbit() {
@@ -17,12 +16,17 @@ export default class OrbitingObject extends GameObject {
     this.translate(xTranslation, yTranslation, 0.0);
   }
 
+  pulse() {
+    const amplitude = 0.25;
+    const offset = 0.5;
+    const period = 3.0;
+    const scale = amplitude * Math.sin(period * this.timeObject.t) + offset;
+    this.scale(scale);
+  }
+
   update() {
     this.modelMatrix.set();
+    this.pulse();
     this.orbit();
-    // this.modelMatrix.set();
-    // this.scale(this.scale);
-    // this.rotate(this.timeObject.t);
-    // this.translate(0, 0, 0);
   }
 }
