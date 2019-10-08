b3b6d6410636071fd12404e299c98940a3830631
bcarlborg <bcarlborg@gmail.com>
2019-10-08 20:23:39 +0200

    slight update to simple server

--- simpleServer.js
+++ simpleServer.js
@@ -3,4 +3,5 @@ app = express(),
 port = process.env.PORT || 4000;
 // console.log('your site is running at port: ', port);
 app.use(express.static(__dirname + '/src'));
+app.use('/WebGlMath', express.static(__dirname + '/src/WebGlMath'));
 app.listen(port);
