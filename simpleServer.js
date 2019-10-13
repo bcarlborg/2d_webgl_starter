41549a6a37ef038ae6e2d9c62c729f07eae01df1
bcarlborg <bcarlborg@gmail.com>
2019-10-13 16:33:29 +0200

    renaming webglmath to be properly capitalized

--- simpleServer.js
+++ simpleServer.js
@@ -4,6 +4,6 @@ port = process.env.PORT || 4000;
 // console.log('your site is running at port: ', port);
 app.use(express.static(__dirname + '/src'));
 app.use(express.static(__dirname + '/src/lib'));
-app.use(express.static(__dirname + '/src/lib/WebGlMath'));
-app.use(express.static(__dirname + '/src/lib/WebGlMath/js'));
+app.use(express.static(__dirname + '/src/lib/WebGLMath'));
+app.use(express.static(__dirname + '/src/lib/WebGLMath/js'));
 app.listen(port);
