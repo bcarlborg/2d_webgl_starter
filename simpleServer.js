1079431d3ba4c2f233da3bc5d09eb6db75ec327b
bcarlborg <bcarlborg@gmail.com>
2019-10-13 16:17:37 +0200

    hopefully heroku can find my beautius js now

--- simpleServer.js
+++ simpleServer.js
@@ -4,5 +4,5 @@ port = process.env.PORT || 4000;
 // console.log('your site is running at port: ', port);
 app.use(express.static(__dirname + '/src'));
 app.use(express.static(__dirname + '/src/lib'));
-app.use(express.static(__dirname + '/src/lib/WebGlMath'));
+app.use(express.static(__dirname + '/src/lib/WebGlMath/js'));
 app.listen(port);
