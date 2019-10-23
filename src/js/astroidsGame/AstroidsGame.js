654c4654ae11f529d657647f0c3409a2c4837fc9
bcarlborg <bcarlborg@gmail.com>
2019-10-23 18:35:25 +0200

    creating the collider

--- src/js/astroidsGame/AstroidsGame.js
+++ src/js/astroidsGame/AstroidsGame.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import GameObjectBuilder from './gameObjects/GameObjectBuilder.js';
+import Collider from './Collider.js';
 import ClickHandler from '../ClickHandler.js';
 import KeyHandler from '../KeyHandler.js';
 
@@ -12,8 +13,8 @@ export default class AstroidsGame {
     this.gameObjectBuilder = new GameObjectBuilder(this.gl, materialBuilder);
 
     this.gameObjects = [];
-    this.collidableObjects = [];
     this.initializeSystem();
+    this.collider = new Collider(this.gameObjects);
   }
 
   initializeSystem() {
@@ -27,69 +28,12 @@ export default class AstroidsGame {
 
     const spaceShip = this.gameObjectBuilder.newSpaceShip();
     this.gameObjects.push(spaceShip);
-
-    this.collidableObjects = [];
-    this.gameObjects.forEach((obj) => {
-      if (obj.collidable) this.collidableObjects.push(obj);
-    });
   }
 
   update() {
-    this.executeCollider();
+    this.collider.handleAllCollisions();
   }
 
-  executeCollider() {
-    for (let i = 0; i < this.collidableObjects.length; i++) {
-      for (let j = i + 1; j < this.collidableObjects.length; j++) {
-        const obj1 = this.collidableObjects[i];
-        const obj2 = this.collidableObjects[j];
-        const distance = Math.sqrt(
-          (obj1.position.x - obj2.position.x) ** 2 + (obj1.position.y - obj2.position.y) ** 2,
-        );
-
-        // if distance is less than the 2 radius combined then do the below
-        if (distance < obj1.collisionCircleRadius + obj2.collisionCircleRadius) {
-          const normalVecX = (obj2.position.x - obj1.position.x) / distance;
-          const normalVecY = (obj2.position.y - obj1.position.y) / distance;
-          const p = (
-            2 * (
-              obj1.velocity.x * normalVecX
-            + obj1.velocity.y * normalVecY
-            - obj2.velocity.x * normalVecX
-            - obj2.velocity.y * normalVecY
-            )
-          ) / (1 / obj1.invMass + 1 / obj2.invMass);
-
-          obj1.setVelocity(
-            obj1.velocity.x - p * (1 / obj1.invMass) * normalVecX,
-            obj1.velocity.y - p * (1 / obj1.invMass) * normalVecY,
-            0.0,
-          );
-          obj2.setVelocity(
-            obj2.velocity.x + p * (1 / obj2.invMass) * normalVecX,
-            obj2.velocity.y + p * (1 / obj2.invMass) * normalVecY,
-            0.0,
-          );
-
-          const midpointX = (obj1.position.x + obj2.position.x) / 2;
-          const midpointY = (obj1.position.y + obj2.position.y) / 2;
-
-          const obj1X = midpointX
-            + (obj1.collisionCircleRadius * (obj1.position.x - obj2.position.x)) / distance;
-          const obj1Y = midpointY
-            + (obj1.collisionCircleRadius * (obj1.position.y - obj2.position.y)) / distance;
-
-          const obj2X = midpointX
-            + (obj2.collisionCircleRadius * (obj2.position.x - obj1.position.x)) / distance;
-          const obj2Y = midpointY
-            + (obj2.collisionCircleRadius * (obj2.position.y - obj1.position.y)) / distance;
-
-          obj1.setPosition(obj1X, obj1Y, 0.0);
-          obj2.setPosition(obj2X, obj2Y, 0.0);
-        }
-      }
-    }
-  }
 
   getAllObjects() {
     return this.gameObjects;
